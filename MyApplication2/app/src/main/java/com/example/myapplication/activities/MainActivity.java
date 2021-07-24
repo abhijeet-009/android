0de56437eb64639410f36bdb4a7862608079670d
abhi <mailiiabhi@gmail.com>
2021-07-24 15:19:03 +0530

    first push

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.ListActivity;
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+    public void gotoScreen1(View v){
+        Intent intent = new Intent(this, EventHandlingActivity.class);
+        startActivity(intent);
+
+    }
+    public void gotoScreen2(View v){
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
